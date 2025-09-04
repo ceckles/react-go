856159751dcd74c7f24928b0e4b074d2ec2743a0
Chad <chad1020@gmail.com>
2025-09-04 13:20:34 -0500

    added fiber and air, as well as base route and struct

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v3"
+)
+
+type Todo struct {
+	ID        int    `json:"id"`
+	Completed bool   `json:"completed"`
+	Body      string `json:"body"`
+}
+
+func main() {
+	fmt.Println("Hello, World!")
+	app := fiber.New()
+
+	app.Get("/", func(c fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"msg": "Hello, World!"})
+	})
+
+	log.Fatal(app.Listen(":3000"))
+}
